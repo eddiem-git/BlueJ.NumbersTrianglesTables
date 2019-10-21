89fce10141a21d151b66dea8dd993f3ac73f37a4
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-21 15:08:45 -0400

    lab done

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,56 @@
 
-
-
 public class NumberUtilities {
-
     public static String getRange(int stop) {
-        return null;
+        return NumberUtilities.getRange(0, stop, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return NumberUtilities.getRange(start, stop, 1);
     }
 
-
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        int counter = start;
+        while(counter < stop){
+            result += counter;
+            counter = counter + step;
+        }
+        return result;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        int counter = start;
+        while(counter < stop){
+            if(counter%2==0) {
+                result += counter;
+            }
+            counter = counter + 1;
+        }
+        return result;    
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        int counter = start;
+        while(counter < stop){
+            if(counter%2==1) {
+                result += counter;
+            }
+            counter = counter + 1;
+        }
+        return result;    
     }
 
-
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String result = "";
+        int counter = start;
+        while(counter < stop+1){
+            int valueToAdd =  (int)       (Math.pow(counter, exponent));
+            result += valueToAdd;
+            counter = counter+1;
+        }
+        return result;    
+        //  x = 2^3 // int x = Math.pow(2,3);
     }
 }
